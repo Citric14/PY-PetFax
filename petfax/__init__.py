748c4870fb23dc46c95bac8d4c78f04b6b03a6b2
George <georgehender997@gmail.com>
2023-02-22 21:15:44 -0500

    db doesnt work and idk how to fix lol

--- petfax/__init__.py
+++ petfax/__init__.py
@@ -1,7 +1,14 @@
 from flask import Flask
+from flask_migrate import Migrate
 
 def create_app():
     app = Flask(__name__)
+
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://username:password@localhost:5432/petfax'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  
+
+
 
     @app.route('/')
     def index():
@@ -21,5 +28,10 @@ def create_app():
 
     from . import fact
     app.register_blueprint(fact.bp)
+
+    from . import models 
+    models.db.init_app(app)
+    migrate = Migrate(app, models.db)
+
 
     return app
